af6d3cd0477969a5081b54a4991631b8f4bca5a1
Marco Kurzynski <marco.kurzynski@amd.com>
2026-02-19 20:40:08 +0000

    use finer power caps and wait longer before making adjustments

---
+++ chopper.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eu
+
+# https://unix.stackexchange.com/a/55922
+trap 'killall' INT
+
+killall() {
+  trap '' INT TERM
+  echo "** KILLING CHILDREN **"
+  kill -TERM 0
+  wait
+  echo "** DONE **"
+}
+
+for iter in ./$1/profile_trace/*; do
+  python -m chopper.profile.merge -t $iter/*.json -o $(basename $iter).pkl &
+done
+wait
+
+python -m chopper.profile.merge -p *.pkl -o ts.pkl
--- go.sh
+++ go.sh
@@ -2,5 +2,7 @@
 
 set -eu
 
+# sudo amd-smi reset -G
+
 ./runner/primus-cli container --image rocm/primus:v26.1 --volume $PWD:/workspace/Primus -- train pretrain --config examples/torchtitan/configs/MI300X/llama3.1_8B-BF16-pretrain.yaml
 # ./runner/primus-cli container --image rocm/primus:v26.1 --volume $PWD:/workspace/Primus -- train pretrain --config examples/torchtitan/configs/MI300X/deepseek_v3_16b-BF16-pretrain.yaml
