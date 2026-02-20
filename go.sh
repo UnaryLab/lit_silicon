3550bfafb0838c624419e9789c85dadfa19dddc6
Marco Kurzynski <marco.kurzynski@amd.com>
2026-02-20 01:57:51 +0000

    use compiled version, same variation

--- go.sh
+++ go.sh
@@ -4,5 +4,7 @@ set -eu
 
 # sudo amd-smi reset -G
 
-./runner/primus-cli container --image rocm/primus:v26.1 --volume $PWD:/workspace/Primus -- train pretrain --config examples/torchtitan/configs/MI300X/llama3.1_8B-BF16-pretrain.yaml
-# ./runner/primus-cli container --image rocm/primus:v26.1 --volume $PWD:/workspace/Primus -- train pretrain --config examples/torchtitan/configs/MI300X/deepseek_v3_16b-BF16-pretrain.yaml
+CONFIG="examples/torchtitan/configs/MI300X/deepseek_v3_16b-BF16-pretrain.yaml"
+# CONFIG="examples/torchtitan/configs/MI300X/llama3.1_8B-BF16-pretrain.yaml"
+
+python -m chopper.profile.collect --gpu-telemetry --cpu-telemetry -- ./runner/primus-cli container --image rocm/primus:v26.1 --volume $PWD:/workspace/Primus -- train pretrain --config "$CONFIG"
